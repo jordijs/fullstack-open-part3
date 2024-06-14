166b9f5435c8f8b765d9a18ce969a79621dc4d51
Jordi <jordijs6@gmail.com>
2024-06-14 21:55:29 +0200

    3.5: Phonebook backend step 5

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     {
         "id": 1,
@@ -34,7 +36,7 @@ app.get('/api/persons/:id', (request, response) => {
     const person = persons.find(person => person.id === id)
 
     if (person) {
-            response.json(person)
+        response.json(person)
     } else {
         response.status(404).end()
     }
@@ -47,6 +49,34 @@ app.delete('/api/persons/:id', (request, response) => {
     persons = persons.filter(person => person.id !== id)
 
     response.status(204).end()
+
+})
+
+const generateId = () => {
+    const max = 1000000000000000
+    return Math.floor(Math.random() * max);
+  }
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    console.log(body);
+
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId()
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
 
 })
 
